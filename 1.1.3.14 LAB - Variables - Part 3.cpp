33c3577fdba47e57b83051a81956f3f333fa2807
EithanLlanos <eithanllanosb@gmail.com>
2023-11-01 13:35:05 -0500

    first commit

---
+++ 1.1.3.14 LAB - Variables - Part 3.cpp
@@ -0,0 +1,35 @@
+//Scenario
+//Check the program in the editor.Find all possible compilation errors and logic errors.Fix them.
+//Your version of the program must print the same result as the expected output.
+//Before you use the compiler, try to find the errors only by manual code analysis.
+//If you want to improve the variable names, then do so, but remember that variable names have to be as descriptive as possible, and also as short as possible.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int ip Part1 = 027;
+//	int ip Part2 = 0;
+//	int ip Part3 = 0;
+//	int ip Part4 = 1;
+//
+//	printf("Localhost IP is %d.%d.%d.%d\n", ip Part1, ip Part2, ip Part3, ip Part4);
+//}
+//
+//Expected output
+//Localhost IP is 127.0.0.1
+//
+//
+
+#include <stdio.h>
+int main() {
+	int ip1_1 = 127;
+	int ip1_2 = 0;
+	int ip1_3 = 0;
+	int ip1_4 = 1;
+
+	printf("Localhost IP is %d.%d.%d.%d\n", ip1_1, ip1_2, ip1_3, ip1_4);
+
+}
+
